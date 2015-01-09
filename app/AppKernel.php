c64dadf59922debb5c0380bca7977686f57e4978
Szabados Róbert <polarxrobi@gmail.com>
2015-01-09 01:30:46 +0100

    Symfony LogIn

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new VideoConference\LogInBundle\VideoConferenceLogInBundle(),
+        	new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
