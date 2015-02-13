8544b737d6c5c3fd78d4291f225053a5ff0c1b4e
Szabados Róbert <polarxrobi@gmail.com>
2015-02-13 01:32:34 +0100

    Integration tests

--- src/VideoConference/LogInBundle/Tests/Controller/DefaultControllerTest.php
+++ src/VideoConference/LogInBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,39 @@
 namespace VideoConference\LogInBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Validator\Constraints\UrlValidator;
+use Symfony\Component\Validator\Tests\Constraints\UrlValidatorTest;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
+require_once 'PHPUnit/Autoload.php';
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+class DefaultControllerTest extends WebTestCase {
+	public function testIndex() {
+		$client = static::createClient ();
+		$crawler = $client->request ( 'GET', '/' );
+		//Sikeres-e a request
+		$this->assertTrue($client->getResponse()->isSuccessful());
+		//Tartalmaz-e ilyen elemeket
+		$this->assertTrue ( $crawler->filter ( 'a:contains("Log In")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'a:contains("Register")' )->count () > 0 );
+		//LogIn oldalra vezet-e a link
+		$link = $crawler->selectLink("Log In")->link();
+		$crawler=$client->click ( $link );
+		$this->assertTrue($client->getResponse()->isSuccessful());
+		
+	}
+	public function testLogIn() {
+		$client = static::createClient ();
+		$crawler = $client->request ( 'GET', '/login' );
+		//Sikeres-e a request
+		$this->assertTrue($client->getResponse()->isSuccessful());
+		//Tartalmaz-e ilyen elemeket
+		$this->assertTrue ( $crawler->filter ( 'a:contains("Log In")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'a:contains("Register")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Username")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Password")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Remember me")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'input[type="button"]:contains("Login")' )->count () > 0 );
+		
+		
+	}
 }
