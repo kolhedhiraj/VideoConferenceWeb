ac83eb5eb89448f71ee5845c81a53160d80f1091
Szabados Róbert <polarxrobi@gmail.com>
2015-03-03 00:26:43 +0100

    Tests

--- src/VideoConference/LogInBundle/Tests/Controller/DefaultControllerTest.php
+++ src/VideoConference/LogInBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,9 +10,9 @@ require_once 'PHPUnit/Autoload.php';
 class DefaultControllerTest extends WebTestCase {
 	public function testIndex() {
 		$client = static::createClient ();
-		$client->followRedirects(true);
-		$crawler=$client->request ( 'GET', '/' );
-
+		$client->followRedirects ( true );
+		$crawler = $client->request ( 'GET', '/' );
+		
 		// Sikeres-e a request
 		$this->assertTrue ( $client->getResponse ()->isSuccessful () );
 		
@@ -23,12 +23,12 @@ class DefaultControllerTest extends WebTestCase {
 		// LogIn oldalra vezet-e a link
 		$link = $crawler->selectLink ( "Log In" )->link ();
 		$crawler = $client->click ( $link );
-		$crawler=$client->getResponse ();
+		$crawler = $client->getResponse ();
 		$this->assertEquals ( 200, $crawler->getStatusCode () );
 	}
 	public function testLogIn() {
 		$client = static::createClient ();
-		$client->followRedirects(true);
+		$client->followRedirects ( true );
 		$crawler = $client->request ( 'GET', '/login' );
 		
 		// Sikeres-e a request
@@ -48,19 +48,19 @@ class DefaultControllerTest extends WebTestCase {
 		$form ['_username'] = 'admin';
 		$form ['_password'] = 'test';
 		$client->submit ( $form );
-		$crawler=$client->getResponse ();
-		//print($crawler);
+		$crawler = $client->getResponse ();
+		// print($crawler);
 		$this->assertEquals ( 200, $crawler->getStatusCode () );
 	}
 	public function testRegister() {
 		$client = static::createClient ();
-		$client->followRedirects(true);
+		$client->followRedirects ( true );
 		$crawler = $client->request ( 'GET', '/register' );
 		
 		// Sikeres-e a request
 		$this->assertEquals ( 200, $client->getResponse ()->getStatusCode () );
 		
-		//Tartalmaz-e ilyen elemeket
+		// Tartalmaz-e ilyen elemeket
 		$this->assertTrue ( $crawler->filter ( 'html:contains("Username")' )->count () > 0 );
 		$this->assertTrue ( $crawler->filter ( 'html:contains("Password")' )->count () > 0 );
 		$this->assertTrue ( $crawler->filter ( 'html:contains("Email")' )->count () > 0 );
@@ -71,40 +71,56 @@ class DefaultControllerTest extends WebTestCase {
 		$form = $buttonCrawlerNode->form ();
 		$form ['fos_user_registration_form[username]'] = 'test_user';
 		$form ['fos_user_registration_form[email]'] = 'test_email@test.com';
-		$form ['fos_user_registration_form[plainPassword][first]'] = 'test_password';		
-		$form ['fos_user_registration_form[plainPassword][second]'] = 'test_password';
-		//print(implode(" ",$form->getValues()));
-		$client->submit ( $form );
-		$crawler=$client->getResponse ();
-		//print($crawler);
-		$this->assertEquals ( 200, $crawler->getStatusCode () );
-		
-	}
-	
-	public function testManageRooms() {
-		$client = static::createClient ();
-		$client->followRedirects(true);
-		$crawler = $client->request ( 'GET', '/manage_rooms' );
-	
-		// Sikeres-e a request
-		$this->assertEquals ( 200, $client->getResponse ()->getStatusCode () );
-	
-		//Tartalmaz-e ilyen elemeket
-		$this->assertTrue ( $crawler->filter ( 'html:contains("Name")' )->count () > 0 );
-		$this->assertTrue ( $crawler->filter ( 'html:contains("Password")' )->count () > 0 );
-		
-		// Form teszt
-		$buttonCrawlerNode = $crawler->selectButton ( 'Register' );
-		$form = $buttonCrawlerNode->form ();
-		$form ['fos_user_registration_form[username]'] = 'test_user';
-		$form ['fos_user_registration_form[email]'] = 'test_email@test.com';
 		$form ['fos_user_registration_form[plainPassword][first]'] = 'test_password';
 		$form ['fos_user_registration_form[plainPassword][second]'] = 'test_password';
-		//print(implode(" ",$form->getValues()));
+		// print(implode(" ",$form->getValues()));
 		$client->submit ( $form );
-		$crawler=$client->getResponse ();
-		//print($crawler);
+		$crawler = $client->getResponse ();
+		// print($crawler);
 		$this->assertEquals ( 200, $crawler->getStatusCode () );
-	
+	}
+	public function testManageProfile() {
+		
+		//Bejelentkezés
+		$client = static::createClient ();
+		$client->followRedirects ( true );
+		$crawler=$client->request('GET','/login');
+		$buttonCrawlerNode = $crawler->selectButton ( '_submit' );
+		$form = $buttonCrawlerNode->form ();
+		$form ['_username'] = 'admin';
+		$form ['_password'] = 'test';
+		$client->submit ( $form );
+		
+		//Edit profile
+		$crawler = $client->request ( 'GET', '/profile/edit' );
+		$this->assertEquals ( 200, $client->getResponse ()->getStatusCode () );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Username")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Email")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Current password")' )->count () > 0 );
+		// ********************
+		// ***IDE MÉG KÓDOT****
+		// ********************
+	}
+	public function testCreateRoom() {
+		
+		//Bejelentkezés
+		$client = static::createClient ();
+		$client->followRedirects ( true );
+		$crawler=$client->request('GET','/login');
+		$buttonCrawlerNode = $crawler->selectButton ( '_submit' );
+		$form = $buttonCrawlerNode->form ();
+		$form ['_username'] = 'admin';
+		$form ['_password'] = 'test';
+		$client->submit ( $form );
+		
+		//Create room
+		$crawler = $client->request ( 'GET', '/create_room' );
+		$this->assertEquals ( 200, $client->getResponse ()->getStatusCode () );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Name")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Description")' )->count () > 0 );
+		$this->assertTrue ( $crawler->filter ( 'html:contains("Max users")' )->count () > 0 );
+		// ********************
+		// ***IDE MÉG KÓDOT****
+		// ********************
 	}
 }
